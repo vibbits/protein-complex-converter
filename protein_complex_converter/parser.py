45dce455b93bec7dddffa86a7f15a173d504aa5d
silkeplas <silke.plas@student.howest.be>
2023-05-24 17:50:36 +0200

    Add xml parser for pmids and authors, add sources

--- protein_complex_converter/parser.py
+++ protein_complex_converter/parser.py
@@ -12,6 +12,8 @@ class Row:
     uniprot_ids: list[str]
     pubmed_ids: list[str]
     first_author_name: list[str]
+    source: str
+    taxid: str
 
 @dataclass
 class MitabRow:
@@ -19,6 +21,7 @@ class MitabRow:
     uidb: str
     author: str
     pmids: str
+    sourcedb: str
     
     
 # Extract the identifiers of molecules in complex
@@ -29,16 +32,27 @@ def extract_id(name: str) -> str:
 def extract_pubmed_id(crossref:str) -> str:
     return crossref.split("(")[0] 
 
+# Parse pubmed xml to get first author and publication year
+def get_pubmedArticles(pubmed_ids):
+    pubmed_base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract"
+    response = req.get(pubmed_base_url+'&id='+str(pubmed_ids))
+    xml = response.text
+    pubmedArticleSet = et.fromstring(xml)
+    return xml
+
 # Extract first authors using pubmed ids
-def extract_first_author(pmid_esearch:str) -> str:
-    xml_data = pmid_esearch.read()
+def extract_first_author(xml:str) -> str:
+    xml_data = xml
     soup = BeautifulSoup(xml_data, 'xml')
     author_list = soup.find_all('Author')
     first_author = author_list[0]
     last_name = first_author.find('LastName').text
     publication_year = soup.find('PubDate').find('Year').text
-    last_name_with_year = last_name + " " + "et al." + " " + "(" + publication_year + ")"
+    last_name_with_year = last_name + " " + "et al. " + "(" + publication_year + ")"
     return last_name_with_year
+
+def extract_taxid(taxid:int) -> str:
+    return "taxid:"+taxid+"(Homo sapiens)"
 
 # Get the complex, uniprot and pubmed identifiers and parse first author from pmid
 def parse_complex_tab(ct: str) -> None:
@@ -57,7 +71,9 @@ def parse_complex_tab(ct: str) -> None:
                 "Identifiers (and stoichiometry) of molecules in complex"
             ].split("|")],
             pubmed_ids= '|'.join(pubmed_ids),
-            first_author_name = '|'.join(first_author_name)
+            first_author_name = '|'.join(first_author_name),
+            source = row["Source"],
+            taxid = extract_taxid(taxid) for taxid in row["Taxonomy identifier"]
         ))
     return rows
 
@@ -69,15 +85,17 @@ def convert_to_mitab(rows: list[Row]) -> list[MitabRow]:
                 uida = row.complex, 
                 uidb = id,
                 author = row.first_author_name,
-                pmids = row.pubmed_ids
+                pmids = row.pubmed_ids,
+                sourcedb = row.source
             )
 
 # Serialize the mitab results
 def serialize_to_mitab(rows: Iterable[MitabRow]) -> str:
     mitab = StringIO() # StringIO: set string as object file
-    writer = csv.DictWriter(mitab, fieldnames=["#uidA","uidB","author","pmids"], dialect="excel-tab")
+    writer = csv.DictWriter(mitab, fieldnames=["#uidA","uidB","author","pmids","sourcedb"], dialect="excel-tab", quoting=csv.QUOTE_NONE, quotechar='')
     writer.writeheader()
     for row in rows:
-        writer.writerow({"#uidA":row.uida,"uidB":row.uidb,"author":row.author,"pmids":row.pmids})
+        writer.writerow({"#uidA":row.uida,"uidB":row.uidb,"author":row.author,"pmids":row.pmids,"sourcedb":row.sourcedb})
+        print(row.sourcedb)
     return mitab.getvalue()
 
